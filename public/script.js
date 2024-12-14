f6528e1783aa0c6c3b86fc01ca78a329908b326b
bilalboultif <80066430+bilalboultif@users.noreply.github.com>
2024-12-14 03:11:54 -0800

    Update script.js with cordova

--- public/script.js
+++ public/script.js
@@ -41,48 +41,29 @@ var peer = new Peer({
   config: {
     'iceServers': [
       { url: 'stun:stun01.sipphone.com' },
-    { url: 'stun:stun.ekiga.net' },
-    { url: 'stun:stunserver.org' },
-    { url: 'stun:stun.softjoys.com' },
-    { url: 'stun:stun.voiparound.com' },
-    { url: 'stun:stun.voipbuster.com' },
-    { url: 'stun:stun.voipstunt.com' },
-    { url: 'stun:stun.voxgratia.org' },
-    { url: 'stun:stun.xten.com' },
-    {url:'stun:stun01.sipphone.com'},
-    {url:'stun:stun.ekiga.net'},
-    {url:'stun:stun.fwdnet.net'},
-    {url:'stun:stun.ideasip.com'},
-    {url:'stun:stun.iptel.org'},
-    {url:'stun:stun.rixtelecom.se'},
-    {url:'stun:stun.schlund.de'},
-    {url:'stun:stun.l.google.com:19302'},
-    {url:'stun:stun1.l.google.com:19302'},
-    {url:'stun:stun2.l.google.com:19302'},
-    {url:'stun:stun3.l.google.com:19302'},
-    {url:'stun:stun4.l.google.com:19302'},
-    {url:'stun:stunserver.org'},
-    {url:'stun:stun.softjoys.com'},
-    {url:'stun:stun.voiparound.com'},
-    {url:'stun:stun.voipbuster.com'},
-    {url:'stun:stun.voipstunt.com'},
-    {url:'stun:stun.voxgratia.org'},
-    {url:'stun:stun.xten.com'},
-    {
-      url: 'turn:numb.viagenie.ca',
-      credential: 'muazkh',
-      username: 'webrtc@live.com'
-    },
-    {
-      url: 'turn:192.158.29.39:3478?transport=udp',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808'
-    },
-    {
-      url: 'turn:192.158.29.39:3478?transport=tcp',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808'
-    }
+      { url: 'stun:stun.ekiga.net' },
+      { url: 'stun:stunserver.org' },
+      { url: 'stun:stun.softjoys.com' },
+      { url: 'stun:stun.voiparound.com' },
+      { url: 'stun:stun.voipbuster.com' },
+      { url: 'stun:stun.voipstunt.com' },
+      { url: 'stun:stun.voxgratia.org' },
+      { url: 'stun:stun.xten.com' },
+      {
+        url: 'turn:numb.viagenie.ca',
+        credential: 'muazkh',
+        username: 'webrtc@live.com'
+      },
+      {
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      },
+      {
+        url: 'turn:192.158.29.39:3478?transport=tcp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      }
     ]
   }
 });
@@ -135,6 +116,7 @@ function getUserMedia() {
       console.error("Error accessing media devices:", err);
     });
 }
+
 // Getting user media for video and audio
 let myVideoStream;
 navigator.mediaDevices
@@ -146,16 +128,16 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
 
-   peer.on('call', (call) => {
-  console.log('Receiving call from:', call.peer);
-  call.answer(myVideoStream);  // Answer the call with your local stream
+    peer.on('call', (call) => {
+      console.log('Receiving call from:', call.peer);
+      call.answer(myVideoStream);  // Answer the call with your local stream
 
-  const video = document.createElement('video');
-  call.on('stream', (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
-  });
-});
-    
+      const video = document.createElement('video');
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
     // Listen for new users connecting
     socket.on("user-connected", (userId) => {
       connectToNewUser(userId, myVideoStream);  // Connect to the new user
